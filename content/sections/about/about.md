f3c1b8924936e2e2f91f906803b4744dfee999ba
somedayfat <somedayfat@gmail.com>
2023-04-26 15:24:38 +0700

    first commit

---
+++ content/sections/about/about.md
@@ -0,0 +1,13 @@
+---
+imageSrc: "../../images/emod.jpg"
+imageAlt: "About Me"
+---
+
+Portfolio Minimal is a Gatsby Theme that creates outstanding one-pages portfolio within minutes!
+
+It has predefined sections for your bio, skills, projects, and contact details. If you are a writer on Medium, you can integrate your latest articles in a distinct section as well. While building the theme, I tried to keep the setup as simple as possible while keeping everything configurable if you like to.
+
+Using theme composition, you can easily customize the CSS theme to your own preferences - e.g. change colors, fonts, etc.
+
+Last but not least, it has some cool features you can opt-in to: Dark Mode, Splash Screen, Cookie Banner, and more to find out :)
+
---
+++ src/pages/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  AboutSection,
+  ArticlesSection,
+  ContactSection,
+  HeroSection,
+  InterestsSection,
+  Page,
+  ProjectsSection,
+  Seo,
+} from "gatsby-theme-portfolio-minimal";
+
+export default function IndexPage() {
+  return (
+    <>
+      <Seo title="Sutrisno bin Junanta" />
+      <Page useSplashScreenAnimation>
+        <HeroSection sectionId="hero" />
+        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
+        <AboutSection sectionId="about" heading="About Me" />
+        <InterestsSection sectionId="details" heading="Details" />
+        <ProjectsSection sectionId="features" heading="Built-in Features" />
+     
+      </Page>
+    </>
+  );
+}
+
+//  // <ContactSection sectionId="github" heading="Issues?" /> //
